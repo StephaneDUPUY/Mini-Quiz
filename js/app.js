03b13cb5008afcd18faaf462efe404b88e94b464
Stéphane DUPUY <38228196+StephaneDUPUY@users.noreply.github.com>
2020-03-18 16:30:04 +0100

    initialisation

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var app = {
+    init: function () {
+        console.log('app.init');
+
+
+    }
+};
+
+// HTML fully loaded and catch it
+document.addEventListener('DOMContentLoaded', app.init);
---
+++ js/questions.js
@@ -0,0 +1,12 @@
+// database
+var questions = [
+    'Combien de joueurs jouent sur un terrain de football (en Ligue 1 par exemple) ?',
+    'Combien font 6 x 12 ?',
+    'L\'eau est composée de combien d\'atomes ?'
+];
+
+var responses = [
+    22,
+    72,
+    3
+];
