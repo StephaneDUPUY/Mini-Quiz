57ff5c94ae72daa45b77c2d6d069bd1a6329f6ee
Stéphane DUPUY <38228196+StephaneDUPUY@users.noreply.github.com>
2020-03-19 08:20:56 +0100

    check user answer with good answer

--- js/app.js
+++ js/app.js
@@ -2,22 +2,48 @@ var app = {
 
     // launched from init() and return answer
     askQuestion: function(questionToAsk){
-    console.log(questionToAsk);
+        console.log(questionToAsk);
 
-    // store and return answer
-    var answer = prompt(questionToAsk);
-    return answer;
+        // store and return answer from popup
+        var answer = prompt(questionToAsk);
+        return answer;
+    },
+
+    checkResponse: function(questionId, userResponse){
+        // store good answer from responses array
+        var correctAnswer = responses[questionId];
+
+        // if answer is good return tru
+        // parseInt to return integer
+        if (correctAnswer === parseInt(userResponse)){
+            return true;
+        }
+        else {
+            return false;
+        }
     },
 
     init: function () {
         console.log('app.init');
 
+        // set manually question number
+        var questionId = 0;
         // store question to ask
-        var question = questions[0];
+        var question = questions[questionId];
 
         // store user answer
         var userAnswer = app.askQuestion(question);
         console.log(userAnswer);
+
+        //isAnswerCorrect contient un booléen
+        var isAnswerCorrect = app.checkResponse(questionId, userAnswer);
+
+        if (isAnswerCorrect){
+        console.log("exact !");
+        }
+        else {
+        console.log("non !");
+    }
 
     }
 };
