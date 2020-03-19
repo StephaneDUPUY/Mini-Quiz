d92ed32c9ac78af8ddde74440ae7e223fb02a3f5
Stéphane DUPUY <38228196+StephaneDUPUY@users.noreply.github.com>
2020-03-19 07:37:26 +0100

    ask first question and store user answer

--- js/app.js
+++ js/app.js
@@ -2,6 +2,14 @@ var app = {
     init: function () {
         console.log('app.init');
 
+        app.askQuestion(questions[0]);
+    },
+
+
+    // new method to give question to user
+    askQuestion: function(questionToAsk){
+        let userAnswer = prompt(questionToAsk);
+        console.log(userAnswer);
 
     }
 };
