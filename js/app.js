aef3ee76ba44330ff7395fa6024ff6a528af8d4e
Stéphane DUPUY <38228196+StephaneDUPUY@users.noreply.github.com>
2020-03-19 11:17:17 +0100

    loop on all questions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = {
 
-    // launched from init() and return answer
+    // launched question and return answer
     askQuestion: function(questionToAsk){
         console.log(questionToAsk);
 
@@ -9,11 +9,12 @@ var app = {
         return answer;
     },
 
+    // check if answer is good
     checkResponse: function(questionId, userResponse){
         // store good answer from responses array
         var correctAnswer = responses[questionId];
 
-        // if answer is good return tru
+        // if answer is good return true
         // parseInt to return integer
         if (correctAnswer === parseInt(userResponse)){
             return true;
@@ -23,36 +24,42 @@ var app = {
         }
     },
 
+    playQuizz: function() {
+        // loop on all questions array
+        for(var i = 0; i < questions.length; i++){
+            console.log('app.init');
+            var questionId = i;
+
+            // store question to ask
+            var question = questions[questionId];
+
+            // store user answer
+            var userAnswer = app.askQuestion(question);
+            console.log(userAnswer);
+
+            //isAnswerCorrect is a boolean
+            var isAnswerCorrect = app.checkResponse(questionId, userAnswer);
+
+            // will content ul where added question
+            var list = null;
+
+            // if good answer
+            if (isAnswerCorrect){
+                // target green ul
+                list = document.querySelector("#right .responses");
+            }
+            else {
+                // else target red ul
+                list = document.querySelector("#wrong .responses");
+            }
+            // and then add li with question in ul
+            list.innerHTML += '<li>' + question + '</li>';
+        }
+
+    },
+
     init: function () {
-        console.log('app.init');
-
-        // set manually question number
-        var questionId = 0;
-        // store question to ask
-        var question = questions[questionId];
-
-        // store user answer
-        var userAnswer = app.askQuestion(question);
-        console.log(userAnswer);
-
-        //isAnswerCorrect is a boolean
-        var isAnswerCorrect = app.checkResponse(questionId, userAnswer);
-
-        // will content ul where added question
-        var list = null;
-
-        // if good answer
-        if (isAnswerCorrect){
-            // target green ul
-            list = document.querySelector("#right .responses"); 
-        }
-        else {
-            // else red one
-            list = document.querySelector("#wrong .responses");
-        }
-        // then add li with question in ul
-        list.innerHTML += '<li>' + question + '</li>';
-
+        app.playQuizz();
     }
 };
 
